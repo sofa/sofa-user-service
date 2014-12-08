7e0f47e445b3eb2dd7366517742f4164348069c9
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2014-12-08 15:53:36 +0100

    chore(*): release 0.5.0

--- dist/sofa.userService.js
+++ dist/sofa.userService.js
@@ -1,5 +1,5 @@
 /**
- * sofa-user-service - v0.4.0 - 2014-06-23
+ * sofa-user-service - v0.5.0 - 2014-12-08
  * http://www.sofa.io
  *
  * Copyright (c) 2014 CouchCommerce GmbH (http://www.couchcommerce.com / http://www.sofa.io) and other contributors
